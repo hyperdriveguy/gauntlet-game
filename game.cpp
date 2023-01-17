56623c09d7cd1aea8a880346b2bbcf13b0fe2205
Carson <hyperdriveguy@gmail.com>
2023-01-16 22:41:26 -0700

    Switch to using an unordered_map instead of struct
    It *should* be more efficent and helps readability and extendability

--- game.cpp
+++ game.cpp
@@ -11,8 +11,6 @@
 // Defines are for some game configuration
 #define CHARACTER_POINTS 20
 
-
-
 using std::string;
 
 enum class StatusEffect {
@@ -39,10 +37,12 @@ class Contestant {
 	public:
 		string name;
 		int level;
-		BaseStats base_stats;
+		std::unordered_map<string, int> base_stats;
 		StatusEffect status;
-		Contestant() : name("Contestant"), level(1), base_stats({1, 1, 1, 1, 1}), status(StatusEffect::Normal) {}
-		Contestant(string *nam, int lev, BaseStats *b_stats) :
+		Contestant() : name("Contestant"), level(1), status(StatusEffect::Normal) {
+			base_stats = {{"health", 1}, {"strength", 1}, {"skill", 1}, {"stamina", 1}, {"speed", 1}};
+		}
+		Contestant(string* nam, int lev, std::unordered_map<string, int>* b_stats) :
 			name(*nam), level(lev), base_stats(*b_stats),
 			status(StatusEffect::Normal) {}
 	// TODO: Methods
@@ -51,7 +51,7 @@ class Contestant {
 class Player : public Contestant {
 	public:
 		std::unordered_map<string, int> inventory;
-		Player(string *nam, int lev, BaseStats *b_stats) :
+		Player(string *nam, int lev, std::unordered_map<string, int>* b_stats) :
 			Contestant(nam, lev, b_stats) {}
 };
 
@@ -101,7 +101,7 @@ int prompt_int(const string prompt_text, const bool same_line = true) {
 }
 
 
-void print_base_stats_table(const BaseStats& base_stats) {
+void print_base_stats_table(std::unordered_map<string, int>& base_stats) {
 	std::cout << std::left << std::setw(15) << "Attribute"
 				<< std::setw(45) << "Description"
 				<< std::right << std::setw(15) << "Level" << std::endl;
@@ -110,37 +110,58 @@ void print_base_stats_table(const BaseStats& base_stats) {
 
 	std::cout << std::left << std::setw(15) << "Health"
 				<< std::setw(45) << "How much damage you can take before you die"
-				<< std::right << std::setw(15) << base_stats.health << std::endl;
+				<< std::right << std::setw(15) << base_stats["health"] << std::endl;
 	std::cout << std::left << std::setw(15) << "Strength"
 				<< std::setw(45) << "Your brute power without weapons"
-				<< std::right << std::setw(15) << base_stats.strength << std::endl;
+				<< std::right << std::setw(15) << base_stats["strength"] << std::endl;
 	std::cout << std::left << std::setw(15) << "Skill"
 				<< std::setw(45) << "How practiced you are in your talents"
-				<< std::right << std::setw(15) << base_stats.skill << std::endl;
+				<< std::right << std::setw(15) << base_stats["skill"] << std::endl;
 	std::cout << std::left << std::setw(15) << "Stamina"
 				<< std::setw(45) << "How long it takes before you get exhausted"
-				<< std::right << std::setw(15) << base_stats.stamina << std::endl;
+				<< std::right << std::setw(15) << base_stats["stamina"] << std::endl;
 	std::cout << std::left << std::setw(15) << "Speed"
 				<< std::setw(45) << "How fast your movement and reactions are"
-				<< std::right << std::setw(15) << base_stats.speed << std::endl;
+				<< std::right << std::setw(15) << base_stats["speed"] << std::endl;
 }
 
 
-BaseStats distribute_stats() {
-	BaseStats base_stats;
+std::unordered_map<string, int> distribute_stats() {
+	std::unordered_map<string, int> base_stats;
+	// Define shortcut letters
+	std::unordered_map<string, string> option_map = {
+		{"h", "health"},
+		{"s", "strength"},
+		{"k", "skill"},
+		{"t", "stamina"},
+		{"p", "speed"}
+	};
 	string confirm = "";
 	do {
 		int points_to_distribute = CHARACTER_POINTS;
-		base_stats = {1, 1, 1, 1, 1};
+		base_stats = {{"health", 1}, {"strength", 1}, {"skill", 1}, {"stamina", 1}, {"speed", 1}};
 		while (points_to_distribute > 0) {
 			clear_output();
 			print_base_stats_table(base_stats);
 			std::cout << "You have " << points_to_distribute << " points to distribute." << std::endl;
-			// TODO: add error checking before prompting for adding points
-			// Prompts
-			string stat_choice = prompt("Which stat would you like to increase? (Health, Strength, sKill, sTamina, sPeed)", true);
-			int points_to_add = prompt_int("How many points would you like to add to this stat?");
 
+			// Stat increase prompt
+			string stat_choice = prompt("Which stat would you like to increase? (Health, Strength, sKill, sTamina, sPeed)", true);
+			// Error and shortcut handling
+			if (stat_choice.length() == 1) {
+				if (option_map.find(stat_choice) == option_map.end()) {
+					message_wait("Not a valid stat shortcut, please try again.");
+					continue;
+				}
+				stat_choice = option_map[stat_choice];
+			}
+			else if (base_stats.find(stat_choice) == base_stats.end()) {
+				message_wait("Not a valid stat, please try again.");
+				continue;
+			}
+
+			// Prompt for number of points
+			int points_to_add = prompt_int("How many points would you like to add to this stat?");
 			// Error check based on number of points available
 			if(points_to_add > points_to_distribute) {
 				message_wait("You don't have that many points to distribute.");
@@ -148,25 +169,8 @@ BaseStats distribute_stats() {
 			}
 
 			// Distribute to respective stat
-			if(stat_choice == "health" || stat_choice == "h") {
-				base_stats.health += points_to_add;
-				points_to_distribute -= points_to_add;
-			} else if(stat_choice == "strength" || stat_choice == "s") {
-				base_stats.strength += points_to_add;
-				points_to_distribute -= points_to_add;
-			} else if(stat_choice == "skill" || stat_choice == "k") {
-				base_stats.skill += points_to_add;
-				points_to_distribute -= points_to_add;
-			} else if(stat_choice == "stamina" || stat_choice == "t") {
-				base_stats.stamina += points_to_add;
-				points_to_distribute -= points_to_add;
-			} else if(stat_choice == "speed" || stat_choice == "p") {
-				base_stats.speed += points_to_add;
-				points_to_distribute -= points_to_add;
-			// Error checking for valid stat entry
-			} else {
-				message_wait("Not a valid stat, please try again.");
-			}
+			base_stats[stat_choice] += points_to_add;
+			points_to_distribute -= points_to_add;
 		}
 		// Show final stats before confirmation
 		clear_output();
@@ -179,12 +183,10 @@ BaseStats distribute_stats() {
 Player new_game() {
 	clear_output();
 	string player_name = prompt("What is your name?", true);
-	BaseStats player_stats = distribute_stats();
+	std::unordered_map<string, int> player_stats = distribute_stats();
 	Player player(&player_name, 1, &player_stats);
-
 	return player;
 }
-
 
 int main() {
 	new_game();
